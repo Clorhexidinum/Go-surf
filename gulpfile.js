581baededc982416341ad5093fbc996536b35be0
Clorhexidinum <mkubekov@gmail.com>
2020-11-02 21:57:54 +0300

    d

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,90 @@
-"use strict";
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var csso = require("gulp-csso");
-var rename = require("gulp-rename");
-var server = require("browser-sync").create();
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
-var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
 
-gulp.task("css", function () {
-  return gulp.src([
-    "source/sass/style.scss",
-    "node_modules/animate.css/animate.css",
-])
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
+gulp.task('clean', async function(){
+  del.sync('dist')
+})
 
-gulp.task("normalize", function () {
-  return gulp.src("source/css/normalize.css")
-    .pipe(rename("normalize.min.css"))
-    .pipe(gulp.dest("build/css"))
-});
-
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false,
-    index: "index.html"
-  });
-  gulp.watch("source/scss/*/*.scss", gulp.series("css"));
-  // gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/*.html").on('change', browserSync.reload);
-  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
-});
-
-gulp.task("images", function () {
-  return gulp.src("source/images/*/.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/images"));
-});
-
-gulp.task("webp", function () {
-  return gulp.src([
-    "source/images/*/.{png,jpg}",
-    "!source/images/ignored_by_webp/",
-    "!source/images/ignored_by_webp/*.{png,jpg}"
-  ])
-    .pipe(webp({quality: 75}))
-    .pipe(gulp.dest("buld/img"));
-});
-
-gulp.task("html", function() {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("js", function () {
-  return gulp.src('source/js/*.js')
-    .pipe(uglify())
-    .pipe(rename(function (path) {
-      path.basename += ".min";
-      path.extname = ".js";
+gulp.task('scss', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['last 8 versions']
     }))
-    .pipe(gulp.dest("build/js"));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("copy", function () {
+gulp.task('css', function(){
   return gulp.src([
-    "source/css/style.css",
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/*.ico"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/animate.css/animate.css',
+  ])
+    .pipe(concat('_libs.scss'))
+    .pipe(gulp.dest('app/scss'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("clean", function () {
-  return del("build");
+gulp.task('html', function(){
+  return gulp.src('app/*.html')
+  .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
+gulp.task('script', function(){
+  return gulp.src('app/js/*.js')
+  .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("gulp", gulp.series("css", "server"));
-gulp.task("build", gulp.series("clean", "copy", "images", "webp", "normalize", "css", "html", "js"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task('js', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "app/"
+      }
+  });
+});
+
+gulp.task('export', function(){
+  let buildHtml = gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist'));
+
+  let BuildCss = gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('dist/css'));
+
+  let BuildJs = gulp.src('app/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+    
+  let BuildFonts = gulp.src('app/fonts/**/*.*')
+    .pipe(gulp.dest('dist/fonts'));
+
+  let BuildImg = gulp.src('app/img/**/*.*')
+    .pipe(gulp.dest('dist/img'));   
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/*.html', gulp.parallel('html'))
+  gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('build', gulp.series('clean', 'export'))
+
+gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
