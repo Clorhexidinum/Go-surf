452a41c40d4eb0f08e73247c90b630304fe911e3
Clorhexidinum <mkubekov@gmail.com>
2020-11-03 14:52:01 +0300

    в

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,20 @@
 $(function(){
 
-  $('.header__slider').slick({
-    infinite: true,
-    fade: true,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
-    asNavFor: '.slider-dots',
-  });
+  // $('.header__slider').slick({
+  //   infinite: true,
+  //   fade: true,
+  //   cssEase: 'linear',
+  //   prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+  //   nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+  //   asNavFor: '.slider-dots',
+  // });
+  $('.header__slider').slick();
 
-  $('.slider-dots').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.header__slider',
-  });
+  // $('.slider-dots').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.header__slider',
+  // });
 
   // $('surf-slider').slick({
   //   slidesToShow: 4,
