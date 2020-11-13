ffe9b764f389b8b2e03cd1f8c096ed25dcc4b4b2
Clorhexidinum <mkubekov@gmail.com>
2020-11-13 16:14:54 +0300

    Новая сборка Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,183 @@
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const del = require('del');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const imagemin = require('gulp-imagemin');
+const jpegRecompress = require('imagemin-jpeg-recompress');
+const pngquant = require('imagemin-pngquant');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sourcemap = require('gulp-sourcemaps');
+const rollup = require('gulp-rollup');
+const rename = require("gulp-rename");
+const cache = require('gulp-cache');
+const fs = require('fs');
+const realFavicon = require('gulp-real-favicon');
+const FAVICON_DATA_FILE = 'faviconData.json';
+const browserSync = require('browser-sync').create();
+const config = {
+  server: {
+    baseDir: './build'
+  },
+  tunnel: false,
+  host: 'localhost',
+  port: 3333
+};
+const ghPages = require('gulp-gh-pages');
 
-
-gulp.task('clean', async function(){
-  del.sync('dist')
-})
-
-gulp.task('scss', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer({
-      browsers: ['last 8 versions']
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+gulp.task('clearBuild', function() {
+  return del(['build/*'])
 });
 
-gulp.task('css', function(){
-  return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/animate.css/animate.css',
-  ])
-    .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('app/scss'))
-    .pipe(browserSync.reload({stream: true}))
+gulp.task('html', function () {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('html', function(){
-  return gulp.src('app/*.html')
-  .pipe(browserSync.reload({stream: true}))
+gulp.task('css', function() {
+  return gulp.src(['src/css/normalize.css', 'src/scss/style.scss'])
+    .pipe(sourcemap.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano()
+    ]))
+    .pipe(concat('style.css'))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('../maps'))
+    .pipe(gulp.dest('build/css/'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('script', function(){
-  return gulp.src('app/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('js', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
-  ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "app/"
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemap.init())
+    .pipe(rollup({
+      input: './src/js/main.js',
+      output: {
+        format: 'esm'
       }
+    }))
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(sourcemap.write('../maps'))
+    .pipe(gulp.dest('build/js/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*.*')
+    .pipe(cache(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      jpegRecompress({
+        loops: 5,
+        min: 70,
+        max: 80,
+        quality:'high'
+      }),
+      imagemin.svgo(),
+      imagemin.optipng({optimizationLevel: 3}),
+      pngquant({quality: [.65, .7], speed: 5})
+      ],{ verbose: true }
+    )))
+    .pipe(gulp.dest('build/images/'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*.*')
+    .pipe(gulp.dest('build/fonts/'));
+});
+
+gulp.task('generate-favicon', function(done) {
+  realFavicon.generateFavicon({
+    masterPicture: 'src/images/logo.png',
+    dest: 'build/favicon',
+    iconsPath: 'favicon',
+    design: {
+      ios: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#ffffff',
+        assets: {
+          ios6AndPriorIcons: false,
+          ios7AndLaterIcons: false,
+          precomposedIcons: false,
+          declareOnlyDefaultIcon: true
+        }
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'noChange',
+        backgroundColor: '#ffffff',
+        onConflict: 'override',
+        assets: {
+          windows80Ie10Tile: false,
+          windows10Ie11EdgeTiles: {
+            small: false,
+            medium: true,
+            big: false,
+            rectangle: false
+          }
+        }
+      },
+      androidChrome: {
+        pictureAspect: 'noChange',
+        themeColor: '#ffffff',
+        manifest: {
+          display: 'standalone',
+          orientation: 'notSet',
+          onConflict: 'override',
+          declared: true
+        },
+        assets: {
+          legacyIcon: false,
+          lowResolutionIcons: false
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'silhouette',
+        themeColor: '#ffffff'
+      }
+    },
+    settings: {
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
   });
 });
 
-gulp.task('export', function(){
-  let buildHtml = gulp.src('app/**/*.html')
-    .pipe(gulp.dest('dist'));
-
-  let BuildCss = gulp.src('app/css/**/*.css')
-    .pipe(gulp.dest('dist/css'));
-
-  let BuildJs = gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dist/js'));
-    
-  let BuildFonts = gulp.src('app/fonts/**/*.*')
-    .pipe(gulp.dest('dist/fonts'));
-
-  let BuildImg = gulp.src('app/img/**/*.*')
-    .pipe(gulp.dest('dist/img'));   
+gulp.task('inject-favicon-markups', function() {
+  return gulp.src('src/*.html')
+    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('app/*.html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
+gulp.task('set-favicon', gulp.series('generate-favicon', 'inject-favicon-markups'));
+
+gulp.task('watch', function() {
+  browserSync.init(config);
+  gulp.watch('src/scss/**/*.scss', gulp.series('css'));
+  gulp.watch('src/js/**/*.js', gulp.series('scripts'));
+  gulp.watch('src/*.html', gulp.series('html')).on('change', browserSync.reload);
 });
 
-gulp.task('build', gulp.series('clean', 'export'))
+gulp.task('build',
+  gulp.series('clearBuild',
+    gulp.parallel('html', 'css', 'scripts', 'images', 'fonts')
+  )
+);
 
-gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
+gulp.task('deploy', function() {
+  return gulp.src('build/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('default', gulp.series('build', 'watch'));
